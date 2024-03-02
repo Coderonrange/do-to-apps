bb70f4b69a28076f3a156097bad162f850a3701a
Coderonranger <160169893+Coderonrange@users.noreply.github.com>
2024-03-02 23:25:19 +0530

    doto app

---
+++ do-to-apps/index.js
@@ -0,0 +1,19 @@
+let inputs = document.getElementById("inp");
+let text = document.querySelector(".text");
+
+function add(){
+    if (inputs.value == "") {
+        alert("User pleace Enter Your Task")
+    }
+    else{
+        let newEle=document.createElement("ul");
+        newEle.innerHTML=`${inputs.value} <i class="fa-solid fa-delete-left"></i>`;
+        text.appendChild(newEle);
+        inputs.value="";
+
+        newEle.querySelector("i").addEventListener("click",remove);
+        function remove(){
+            newEle.remove()
+        }
+    }
+}
